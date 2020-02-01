7b3bd12a6b29fd2fe29f0666a7933c8aaed428a5
Alicia Alagala <aliciaalagala@Alicias-MacBook-Pro.local>
2020-02-01 12:00:46 -0600

    Completed 205-tip-calculator.js

--- exercises/205-tip-calculator.js
+++ exercises/205-tip-calculator.js
@@ -10,7 +10,18 @@
 // tipAmount(100, 'good') --> 20
 // tipAmount(40, 'fair') --> 6
 
+function tipAmount (billAmount, levelOfService) {
 
+if (levelOfService === 'good') {
+    return billAmount * (0.2)
+} else if (levelOfService === 'fair') {
+    return billAmount * (0.15)
+} else if (levelOfService === 'poor') {
+    return billAmount * (0.1)
+}
+}
+
+tipAmount(40, 'good')
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "totalAmount" that takes the same arguments as "tipAmount"
@@ -22,6 +33,20 @@
 // totalAmount(40, 'fair') --> 46
 
 
+function totalAmount (billAmount, levelOfService) {
+
+    if (levelOfService === 'good') {
+        return billAmount + tipAmount(billAmount, levelOfService)
+    } else if (levelOfService === 'fair') {
+        return billAmount + tipAmount(billAmount, levelOfService)
+    } else if (levelOfService === 'poor') {
+        return billAmount + tipAmount(billAmount, levelOfService)
+    }
+    
+
+}
+
+
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "splitAmount" that takes a bill amount, the level of service,
@@ -31,3 +56,16 @@
 // Examples:
 // splitAmount(100, 'good', 5) --> 24
 // splitAmount(40, 'fair', 2) --> 23
+
+
+function splitAmount (billAmount, levelOfService, numberOfPeople) { 
+
+    if (levelOfService === 'good') {
+        return (billAmount + tipAmount(billAmount, levelOfService))/numberOfPeople
+    } else if (levelOfService === 'fair') {
+        return (billAmount + tipAmount(billAmount, levelOfService))/numberOfPeople
+    } else if (levelOfService === 'poor') {
+        return (billAmount + tipAmount(billAmount, levelOfService))/numberOfPeople
+    }
+
+}
